6a7723a5907829cc3b904032632937a2a304bd9e
Maneren <maneren731@gmail.com>
2025-01-05 15:50:29 +0100

    python 2024/day19

---
+++ python/2024/day19/part1.py
@@ -0,0 +1,19 @@
+import functools
+from typing import Callable
+
+
+def solve(print: Callable, print_output: Callable) -> None:
+    lines = open(0).read().splitlines()
+    patterns = tuple(lines[0].split(", "))
+
+    @functools.cache
+    def contruct_pattern(desired, current) -> bool:
+        return any(
+            (test := current + pattern) == desired
+            or desired.startswith(test)
+            and contruct_pattern(desired, test)
+            for pattern in patterns
+        )
+
+    total = sum(contruct_pattern(patt, "") for patt in lines[2:])
+    print_output(total)
