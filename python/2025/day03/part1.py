f5c814dec941197b67b68cebd09ee534040e8293
Maneren <maneren731@gmail.com>
2025-12-03 13:12:17 +0100

    python 2025/day03 clean

--- python/2025/day03/part1.py
+++ python/2025/day03/part1.py
@@ -4,16 +4,11 @@ from typing import Callable
 def solve(print: Callable, print_output: Callable) -> None:
     lines = open(0).read().splitlines()
 
-    banks = [list(map(int, line)) for line in lines]
+    banks = (list(map(int, line)) for line in lines)
 
-    total = 0
-
-    for bank in banks:
-        best = 0
-        for i, hi in enumerate(bank):
-            for lo in bank[i + 1 :]:
-                best = max(best, hi * 10 + lo)
-
-        total += best
+    total = sum(
+        max(hi * 10 + lo for i, hi in enumerate(bank) for lo in bank[i + 1 :])
+        for bank in banks
+    )
 
     print_output(total)
