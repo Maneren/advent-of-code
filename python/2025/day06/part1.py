544b07d7981c665fb70dcb7e6b5d2758c71f6daa
Maneren <maneren731@gmail.com>
2025-12-06 14:17:19 +0100

    python 2025/day06 clean

--- python/2025/day06/part1.py
+++ python/2025/day06/part1.py
@@ -1,34 +1,16 @@
-from functools import reduce
-import operator
+from math import prod
 import re
 from typing import Callable
-
-
-def identity(a):
-    return a
-
-
-def strop(op):
-    match op:
-        case "+":
-            return operator.add
-        case "*":
-            return operator.mul
-        case _:
-            raise ValueError(op)
 
 
 def solve(print: Callable, print_output: Callable) -> None:
     lines = open(0).read().splitlines()
 
-    numbers = [re.findall(r"\d+", line) for line in lines[:-1]]
-    ops = map(strop, re.findall(r"[+*]", lines[-1]))
+    numbers = (re.findall(r"\d+", line) for line in lines[:-1])
 
-    total = 0
-
-    for j, op in enumerate(ops):
-        result = reduce(op, (int(num_line[j]) for num_line in numbers))
-        print(result)
-        total += result
+    total = sum(
+        {"+": sum, "*": prod}[op](map(int, operands))
+        for op, operands in zip(re.findall(r"[+*]", lines[-1]), zip(*numbers))
+    )
 
     print_output(total)
--- python/2025/day06/part2.py
+++ python/2025/day06/part2.py
@@ -1,59 +1,13 @@
-from functools import reduce
-import operator
 import re
 from typing import Callable
-
-
-def identity(a):
-    return a
-
-
-def strop(op):
-    match op:
-        case "+":
-            return operator.add
-        case "*":
-            return operator.mul
-        case _:
-            raise ValueError(op)
 
 
 def solve(print: Callable, print_output: Callable) -> None:
     lines = open(0).read().splitlines()
 
-    numbers = lines[:-1]
-    ops = map(strop, re.findall(r"[+*]", lines[-1]))
+    blocks = "\n".join(map(str.strip, map("".join, zip(*lines[:-1])))).split("\n\n")
+    ops = re.findall(r"[+*]", lines[-1])
 
-    print(numbers)
-
-    total = 0
-
-    operands = []
-
-    for column in zip(*numbers):
-        print(column)
-        if all(d == " " for d in column):
-            op = next(ops)
-            result = reduce(op, operands)
-            print(operands, op, "=", result)
-            total += result
-            operands.clear()
-            continue
-
-        power = 1
-        operand = 0
-        for d in reversed(column):
-            if d != " ":
-                operand += int(d) * power
-                power *= 10
-
-        operands.append(operand)
-
-        power *= 10
-
-    op = next(ops)
-    result = reduce(op, operands)
-    print(operands, op, "=", result)
-    total += result
+    total = sum(eval(op.join(block.splitlines())) for op, block in zip(ops, blocks))
 
     print_output(total)
