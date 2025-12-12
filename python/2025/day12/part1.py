ab1421c8d52fe023599d5e4de698db981b95c056
Maneren <maneren731@gmail.com>
2025-12-12 15:55:39 +0100

    python 2025/day12 clean

--- python/2025/day12/part1.py
+++ python/2025/day12/part1.py
@@ -3,38 +3,16 @@ from typing import Callable
 
 
 def solve(print: Callable, print_output: Callable) -> None:
-    lines = open(0).read()
-
-    blocks = lines.split("\n\n")
-
-    shape_blocks, regions_block = blocks[:-1], blocks[-1]
-
-    shapes = []
-
-    for shape_block in shape_blocks:
-        _, shape = shape_block.split("\n", maxsplit=1)
-
-        shape = [[c == "#" for c in line] for line in shape.splitlines()]
-        shapes.append(shape)
+    regions_block = open(0).read().split("\n\n")[-1]
 
     regions = [
         (int(a), int(b), tuple(map(int, c.split())))
         for a, b, c in re.findall(r"(\d++)x(\d++): ((?:\d++ ?+)++)", regions_block)
     ]
 
-    valid = 0
-
-    for width, height, required_shapes in regions:
-        total_required = 0
-
-        for i, count in enumerate(required_shapes):
-            tiles = sum(map(sum, shapes[i]))
-
-            total_required += count * tiles
-
-        if total_required > width * height:
-            continue
-
-        valid += 1
+    valid = sum(
+        9 * sum(required_shapes) <= width * height
+        for width, height, required_shapes in regions
+    )
 
     print_output(valid)
